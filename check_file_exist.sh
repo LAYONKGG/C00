85e3c7eb1aab35f745360b755d85b02182952e2a
LAYONKGG <mlaouali374@gmail.com>
2025-05-08 04:41:38 +0100

    Mise à jour du script check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,11 @@
+“`bash
 #!/data/data/com.termux/files/usr/bin/bash
 read -p "Entrez le nom du fichier à vérifier : " filename
-if [ -z "filename" ]; then
+if [ -z "$filename" ]; then
     echo "Aucun nom de fichier saisi."
-elif [ -f "filename" ]; then
-    echo "Le fichier 'filename' existe."
+elif [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
 else
-    echo "Le fichier 'filename' n'existe pas."
+    echo "Le fichier '$filename' n'existe pas."
 fi
 ```
