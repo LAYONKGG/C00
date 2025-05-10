056e2035aec8e4ce035b7629094a1267904089d2
LAYONKGG <mlaouali374@gmail.com>
2025-05-10 16:38:59 +0100

    Suppression des colonnes vides du script de vérification de fichier

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,9 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
 #Demande à l'utilisateur d'entrer le nom du fichier
 echo "Entrez le nom du fichier à vérifier : "
 read filename
-
 #Vérifie si le fichier existe
 if [ -f "$filename" ]; then
     echo "Le fichier '$filename' existe."
