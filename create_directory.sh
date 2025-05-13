aeb59e0b44d0b56b954da4b5728e75ebff6d5498
LAYONKGG <mlaouali374@gmail.com>
2025-05-13 22:16:34 +0100

    Modification du script de création de répertoire

--- create_directory.sh
+++ create_directory.sh
@@ -1,12 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/bash
-#Demande le nom du dossier
-echo "Entrez le nom du répertoire à créer : "
-read directory_name
-#Vérifie si un nom a été donné
-if [ -n "$directory_name" ]; then
-    # Crée le dossier
-    mkdir "$directory_name"
-    echo "Le répertoire '$directory_name' a été créé."
-else
-    echo "Aucun nom saisi."
-fi
+#script permettant la création du répertoire " test_directory "
+mkdir test_directory -p
