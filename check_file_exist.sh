6307dfe9299c882f0597530694d07a98714be4bb
LAYONKGG <mlaouali374@gmail.com>
2025-05-17 19:37:19 +0100

    Modification du script de vérification de fichier

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Demander à l'utilisateur entrer le nom du fichier
+<<<<<<< HEAD
 echo "Entrez le nom du fichier :"
+=======
+echo "Entrez le nom du fichier:" 
+>>>>>>> bf7f0da (Modification du script de vérification de fichier)
 read filename
-#Vérification de l'existence du fichier
+# Vérifie si le fichier existe et est un fichier ordinaire
 if [ -f "$filename" ]; then
-echo "le fichier '$filename' existe."
+    echo "Le fichier '$filename'  existe."
 else
-echo "le fichier '$filename' n'existe pas."
+    echo "Le fichier '$filename' n'existe pas."
 fi
