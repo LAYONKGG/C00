4b011fd520f9e8e80bfbfcc0b9462ea7ca42e0e5
LAYONKGG <mlaouali374@gmail.com>
2025-05-17 15:48:03 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Demander à l'utilisateur entrer le nom du fichier
-echo "entrer le nom du fichier:"
+echo "Entrez le nom du fichier :"
 read filename
 #Vérification de l'existence du fichier
 if [ -f "$filename" ]; then
